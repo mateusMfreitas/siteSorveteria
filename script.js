bca3a90533166c7dc8ec9a3613cd29ac540b4e7b
Mateus Martins Freitas <mateusmfreitas12@gmail.com>
2021-11-28 11:31:43 -0300

    Revert "Criar carrinho"

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-
 document.getElementById("btnConfirmar").onclick=function(){
     let frmChk=document.getElementById('frmCheckbox').elements;
-    let sabor
 
     for(i =0;i<frmChk.length;i++){
         if(frmChk[i].checked){
-            console.log(frmChk[i].value)
-            if(frmChk[i].value==1){
-                sabor = "morango"
-            }else if(frmChk[i].value==2){
-                sabor = "chocolate"
-            }else{
-                sabor = "creme"
-            }
-            adicionarCarrinho(sabor, 20)
-
+            console.log(frmChk[i])
         }
     }
     return false
 }
-
-function adicionarCarrinho(produto,valor){
-    localStorage.setItem("produto", produto)
-    localStorage.setItem("valor", valor)
-    mostrarCarrinho()
-
-
-}
-function mostrarCarrinho(){
-    for(var i in localStorage) {
-       // console.log(i + ' = ' + localStorage[i]);
-        document.getElementById("textoCarrinho").innerHTML+=localStorage.getItem("produto")
-        console.log(localStorage.key(0))
-    }
-}
-
